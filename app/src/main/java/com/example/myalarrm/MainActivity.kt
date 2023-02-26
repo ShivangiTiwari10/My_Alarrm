3eb9d1ad86cca70ce94295d31dd9ab24cd91c409
Shivangi <shivangit1111@gmail.com>
2023-02-26 21:05:23 +0530

    feat:Make top_toolBar

--- app/src/main/java/com/example/myalarrm/MainActivity.kt
+++ app/src/main/java/com/example/myalarrm/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.myalarrm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+            R.id.ScreenSaver -> Toast.makeText(
+                this,
+                "You clicked on ScreenSaver",
+                Toast.LENGTH_SHORT
+            ).show()
+
+
+            R.id.Settings -> Toast.makeText(this, "You clicked on Settings", Toast.LENGTH_SHORT)
+                .show()
+
+            R.id.privacy -> Toast.makeText(this, "You clicked on privacy", Toast.LENGTH_SHORT)
+                .show()
+
+            R.id.sendFeedback -> Toast.makeText(this, "You clicked on sendFeedback", Toast.LENGTH_SHORT)
+                .show()
+
+
+            R.id.help -> Toast.makeText(this, "You clicked on help", Toast.LENGTH_SHORT)
+                .show()
+        }
+        return true
+    }
+
 }
