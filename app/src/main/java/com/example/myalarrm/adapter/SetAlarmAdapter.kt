45dec2bf532dff280fb3cb9d4d6e0c347e6bbe90
Shivangi <shivangit1111@gmail.com>
2023-03-01 17:34:38 +0530

    feat: write code for adapter

--- app/src/main/java/com/example/myalarrm/adapter/SetAlarmAdapter.kt
+++ app/src/main/java/com/example/myalarrm/adapter/SetAlarmAdapter.kt
@@ -1,4 +1,23 @@
 package com.example.myalarrm.adapter
 
-class SetAlarmAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myalarrm.databinding.AlarmItemBinding
+
+class SetAlarmAdapter(): RecyclerView.Adapter<SetAlarmAdapter.SetAlarmViewHolder>() {
+
+    inner class SetAlarmViewHolder(var binding: AlarmItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SetAlarmViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SetAlarmViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
