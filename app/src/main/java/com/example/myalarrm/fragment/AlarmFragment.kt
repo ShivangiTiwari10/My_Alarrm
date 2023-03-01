e086ef0eddebb09bb4ad133873c4cad5078dd34d
Shivangi <shivangit1111@gmail.com>
2023-03-01 15:21:54 +0530

    refactor :arrange alarm item in proper manner

--- app/src/main/java/com/example/myalarrm/fragment/AlarmFragment.kt
+++ app/src/main/java/com/example/myalarrm/fragment/AlarmFragment.kt
@@ -5,18 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.myalarrm.R
+import com.example.myalarrm.databinding.FragmentAlarmBinding
 
 
 class AlarmFragment : Fragment() {
 
-
+     private lateinit var binding: FragmentAlarmBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alarm, container, false)
+
+        binding = FragmentAlarmBinding.inflate(layoutInflater)
+        return binding.root
     }
 
 
