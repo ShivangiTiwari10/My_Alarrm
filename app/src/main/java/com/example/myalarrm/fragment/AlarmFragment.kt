04abc6e343341cfc91dad4d918a7e7c9db3c4824
Shivangi <shivangit1111@gmail.com>
2023-03-05 13:04:49 +0530

    refactor :Add TimePickerDialog

--- app/src/main/java/com/example/myalarrm/fragment/AlarmFragment.kt
+++ app/src/main/java/com/example/myalarrm/fragment/AlarmFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myalarrm.fragment
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,14 +11,79 @@ import com.example.myalarrm.databinding.FragmentAlarmBinding
 
 class AlarmFragment : Fragment() {
 
-     private lateinit var binding: FragmentAlarmBinding
+
+    private lateinit var binding: FragmentAlarmBinding
+
+
+    private val timePickerDialogListener: TimePickerDialog.OnTimeSetListener =
+        TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute -> // logic to properly handle
+            // the picked timings by user
+            val formattedTime: String = when {
+                hourOfDay == 0 -> {
+                    if (minute < 10) {
+                        "${hourOfDay + 12}:0${minute} am"
+                    } else {
+                        "${hourOfDay + 12}:${minute} am"
+                    }
+                }
+                hourOfDay > 12 -> {
+                    if (minute < 10) {
+                        "${hourOfDay - 12}:0${minute} pm"
+                    } else {
+                        "${hourOfDay - 12}:${minute} pm"
+                    }
+                }
+                hourOfDay == 12 -> {
+                    if (minute < 10) {
+                        "${hourOfDay}:0${minute} pm"
+                    } else {
+                        "${hourOfDay}:${minute} pm"
+                    }
+                }
+                else -> {
+                    if (minute < 10) {
+                        "${hourOfDay}:${minute} am"
+                    } else {
+                        "${hourOfDay}:${minute} am"
+                    }
+                }
+            }
+
+            binding.Preview.text = formattedTime
+        }
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-
         binding = FragmentAlarmBinding.inflate(layoutInflater)
+        binding.floatingButton.setOnClickListener {
+
+            val timePicker = TimePickerDialog(
+                // pass the Context
+                requireContext(),
+                // listener to perform task
+                // when time is picked
+                timePickerDialogListener,
+                // default hour when the time picker
+                // dialog is opened
+                12,
+                // default minute when the time picker
+                // dialog is opened
+                10,
+                // 24 hours time picker is
+                // false (varies according to the region)
+                false
+            )
+
+            // then after building the timepicker
+            // dialog show the dialog to user
+            timePicker.show()
+        }
+
+
         return binding.root
     }
 
